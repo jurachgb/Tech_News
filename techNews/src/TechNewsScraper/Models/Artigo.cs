2603ce07c4d55682e903d32b61f4ae9843762187
jurachgb <balsanjurach@gmail.com>
2026-09-15 16:22:33 -0300

    Correção de erro de digitação

--- techNews/src/TechNewsScraper/Models/Artigo.cs
+++ techNews/src/TechNewsScraper/Models/Artigo.cs
@@ -5,7 +5,7 @@ public class NewsArticle
     
     public string Titulo {get;set;}= string.Empty;
     public string Url {get;set;} =string.Empty;
-    public string Tonte {get;set;}=string.Empty;
+    public string Fonte {get;set;}=string.Empty;
     public int Score {get;set;}
     public DateTime Data {get;set;} =DateTime.UtcNow;
     public List<string> Tag {get;set;}=[];
